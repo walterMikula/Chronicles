6f8c8952031f4fc57c143b6da014ecdff8139b78
walterMikula <146771447+walterMikula@users.noreply.github.com>
2023-11-30 15:59:33 -0600

    Update Chronicles of Eldoria.java

--- Chronicles of Eldoria.java
+++ Chronicles of Eldoria.java
@@ -1,1 +1,50 @@
-public class 
+public class ChroniclesOfEldoriaMidpoint {
+    public static void main(String[] args) {
+
+    }
+
+    /*The StartGame method will be used to initialize the game and present the initial story*/
+    public static void startGame() {
+    }
+
+    /*The chooseCharacter method will allow the user to select a type of character
+     * It will list a number of types of characters along with the stats to go with them
+     * the user will choose a character by typing a char representing the type of character */
+    public static char chooseCharacter() {
+        return 0;
+    }
+
+    /*The chooseAction method will present the user with a list of action their character can do
+     * The options will vary depending on the location of the character in game
+     * This could be a challenging method due to the possibilities of branching story paths*/
+    public static void chooseAction() {
+    }
+
+    /*The combatEncounter method will have a parameter type of string (the type of enemy that the user is facing)
+     * The method will manage combat sequences between the character and the enemy
+     * This  could be difficult because it's based on user input
+     * will return a true or false based upon the success or defeat of the user*/
+    public static boolean combatEncounter() {
+        return false;
+    }
+
+    /*The manageInventory method will allow for the user to actively interact with their inventory
+     * The user will have the choice to pick things up and add them to their inventory (could be al ist or an array)\
+     * The user will be able to view their inventory, use consumables, equip a weapon, and throw away items*/
+    public static void manageInventory() {
+    }
+
+    /*The saveProgress method will save the progress of the game so the user can return at another time to finish
+     * This method will capture all the data in the game and write it to a file so the player can resume
+     * Could be a challenging method because not sure how exactly it will work*/
+    public static void saveProgress() {
+    }
+
+    /* The endGame method will be responsible for the conclusion of the game
+    could be used during anypoint in the game
+    it should end the game and display a success or defeat message depending on the user.
+    * */
+    public static void endGame() {
+    }
+
+}
